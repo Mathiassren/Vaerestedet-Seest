7dd897b10fc37ddc3084f487553c96730eb4ece7
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-02-20 22:40:00 +0100

    Added js and global css

---
+++ javascript/dynamics.js
@@ -0,0 +1,14 @@
+const colorElements = document.querySelectorAll('.nav-links li');
+
+colorElements.forEach(colorElement => {
+  colorElement.addEventListener('mouseover', () => {
+    colorElement.style.height = '129px';
+    colorElement.style.marginTop = '0';
+  });
+
+  colorElement.addEventListener('mouseout', () => {
+    colorElement.style.height = '95px';
+    colorElement.style.marginTop = '34px';
+    
+  });
+});

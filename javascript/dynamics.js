7cae265eb0084a5434476a4d894680cfd11a2631
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-03-08 20:39:09 +0100

    Changed the CSS and added animation to Logo via JS

--- javascript/dynamics.js
+++ javascript/dynamics.js
@@ -14,4 +14,16 @@ colorElements.forEach(colorElement => {
   });
 });
 
-/*Image slider*/
+/*H1 Animation*/
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  });
